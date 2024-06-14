4eb4cb4ce57b461c461dfa66274afa1dfe9daf82
LucaDiba <l.diba@protonmail.com>
2024-06-14 17:06:55 -0600

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Calculator } from "@/components/calculator";
+import { Header } from "./components/header";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="p-6 max-w-4xl mx-auto">
-      <Calculator />
-    </div>
+    <>
+      <Header />
+      <div className="p-6 max-w-4xl mx-auto">
+        <Routes>
+          <Route path="/" element={<Calculator />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
