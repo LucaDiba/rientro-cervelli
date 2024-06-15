0f4992e7e9bfa4c8a3790b4a53950e1329e8c528
LucaDiba <l.diba@protonmail.com>
2024-06-14 18:03:49 -0600

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Calculator } from "@/components/calculator";
-import { Header } from "./components/header";
+import { Header } from "@/components/header";
 import { Route, Routes } from "react-router-dom";
+import { Footer } from "@/components/footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Calculator />} />
         </Routes>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+    <div className="container mx-auto px-4 md:px-6 lg:px-8">
+      <footer className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+        <i>
+          The data provided by this calculator is indicative and may not be
+          accurate. Please consult a tax professional for accurate information.
+        </i>
+      </footer>
+    </div>
+  );
+}
